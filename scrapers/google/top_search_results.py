5f3599aac15da85e21501226f28726594852f3df
CharlesCasey23 <charlescaseymartin@gmail.com>
2024-02-26 17:51:07 +0200

    add top-search-results planing

---
+++ scrapers/google/top_search_results.py
@@ -0,0 +1,20 @@
+
+# search auto-suggestions
+# extract the people also asked questions for each search result
+# extract the related searches for each search result
+# extract the top 5 results for each search
+# extract the following for each top result:
+#   - meta description
+#   - meta keywords
+#   - schema mark up
+#   - canonical tag
+#   - header tag
+#   - image alt description
+#   - xml sitemap
+#   - robots.txt
+#   - html structure
+#   - keyword density
+
+class TopSearchResults:
+  def __init__(self):
+    pass
